4c0abbfd94552b4b65c9d0d57e69e552fdc73d4d
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2024-08-25 12:38:06 -0500

    Update src/brod_gssapi_v1.erl
    Co-authored-by: zmstone <zmstone@gmail.com>

--- src/brod_gssapi_v1.erl
+++ src/brod_gssapi_v1.erl
@@ -127,7 +127,7 @@ auth_continue(#{sasl_conn := Conn} = State, {sasl_ok, Challenge}) ->
             Error
     end;
 auth_continue(#{handshake_vsn := 1, sasl_conn := Conn} = State, {sasl_continue, Challenge}) ->
-	case send_sasl_token(State, Challenge) of
+    case send_sasl_token(State, Challenge) of
         {ok, Token} ->
             case sasl_auth:client_step(Conn, Token) of
                 {ok, SaslRes} ->
